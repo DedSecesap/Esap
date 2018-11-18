333ee85a1c52972481293552b9e2755d46dce434
trushantgautam <40313557+trushantgautam@users.noreply.github.com>
2018-11-18 21:02:51 +0530

    Added overhead menu from bottom navigation and checked landscape mode of planner.( Use autorotate for testing).
    Issue- On changed orientation you need to reclick the item selected.... do check out.

---
+++ app/src/main/java/com/example/apple/myapplication/EventClickedFragment.java
@@ -0,0 +1,94 @@
+package com.example.apple.myapplication;
+
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link EventClickedFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link EventClickedFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EventClickedFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public EventClickedFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment EventClickedFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static EventClickedFragment newInstance(String param1, String param2) {
+        EventClickedFragment fragment = new EventClickedFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment.setVisibility(View.VISIBLE);
+        View view=inflater.inflate(R.layout.fragment_event_clicked, container, false);
+        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/com/example/apple/myapplication/EventsFragment.java
+++ app/src/main/java/com/example/apple/myapplication/EventsFragment.java
@@ -1,6 +1,7 @@
 package com.example.apple.myapplication;
 
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -50,6 +51,7 @@ public class EventsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         return inflater.inflate(R.layout.fragment_events, container, false);
     }
 
